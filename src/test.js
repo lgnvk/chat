cad38926b2df4f22f537b4e01fa7beb67a293697
Konstantin Loginov <k.s.loginoff@gmail.com>
2023-02-27 17:43:58 +0400

    task_1

---
+++ src/test.js
@@ -0,0 +1,68 @@
+function get(obj, path, defaultValue) {
+    const keys = path.split('.');
+
+    let result = obj;
+
+    for (let key of keys) {
+        result = result[key];
+
+        if(result === undefined) {
+            return defaultValue;
+        };
+    };
+
+    return result ?? defaultValue;
+};
+
+let testTempl = `
+<div>
+    {{ field1 }}
+    <span>{{field2}}</span>
+    <span>{{ field3.info.name }}</span>
+</div>
+`;
+
+const ctx = {
+    field1: "text1",
+    field2: "text2",
+    field3: {
+        info: {
+            name: "some Name",
+        },
+    },
+};
+
+class Templator {
+    TEMPLATE_REGEXP = /\{\{(.*?)\}\}/gi;
+
+    constructor(template) {
+        this._template = template;
+    }
+
+    compile(ctx) {
+        return this._compileTemplate(ctx);
+    }
+
+    _compileTemplate = (ctx) => {
+        let tmpl = this._template;
+        let key = null;
+        const regExp = this.TEMPLATE_REGEXP;
+
+        while ((key = regExp.exec(tmpl))) {
+            if(key[1]) {
+                const tmplValue = key[1].trim();
+                const data = get(ctx, tmplValue);
+                tmpl = tmpl.replace(new RegExp(key[0], 'gi'), data);
+            }
+        }
+
+        return tmpl;
+    }
+}
+
+const comp = new Templator(testTempl);
+
+const res = comp.compile(ctx);
+
+const root = document.querySelector('#root');
+root.innerHTML = res;
